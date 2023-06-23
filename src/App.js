9381390888f11b14f0821e50baa1862e457479c3
Henryk212 <111775517+Henryk212@users.noreply.github.com>
2023-06-23 19:52:53 -0300

    refatorando estilos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { Globalstyle } from "./Components/Globalstyle";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+
+
 
 function App() {
   return (
     <>
+      <Globalstyle/>
       <Cabecalho />
       <Container />
     </>
